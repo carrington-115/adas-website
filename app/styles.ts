011ba4955b4dd449f44ce54c6608058146b13dcd
carrington-115 <frumarkcarrington@gmail.com>
2024-12-15 11:10:40 +0530

    wrote the style files for the frontend interface

---
+++ app/styles.ts
@@ -0,0 +1,99 @@
+const colorStyle = {
+  primaryColor: "#4B0082",
+  backgroundColor: "#FFFFFF",
+};
+
+const textStyles = {
+  bold: {
+    displayBold: {
+      fontFamily: "Poppins",
+      fontSize: "48px",
+      fontStyle: "normal",
+      fontWeight: "700",
+      lineHeight: "normal",
+    },
+    headingBold: {
+      fontFamily: "Poppins",
+      fontSize: "36px",
+      fontStyle: "normal",
+      fontWeight: "700",
+      lineHeight: "normal",
+    },
+    titleBold: {
+      fontFamily: "Poppins",
+      fontSize: "24px",
+      fontStyle: "normal",
+      fontWeight: "700",
+      lineHeight: "normal",
+    },
+    bodyBold: {
+      fontFamily: "Poppins",
+      fontSize: "16px",
+      fontStyle: "normal",
+      fontWeight: "700",
+      lineHeight: "normal",
+    },
+  },
+  medium: {
+    displayBold: {
+      fontFamily: "Poppins",
+      fontSize: "48px",
+      fontStyle: "normal",
+      fontWeight: "500",
+      lineHeight: "normal",
+    },
+    headingBold: {
+      fontFamily: "Poppins",
+      fontSize: "36px",
+      fontStyle: "normal",
+      fontWeight: "500",
+      lineHeight: "normal",
+    },
+    titleBold: {
+      fontFamily: "Poppins",
+      fontSize: "24px",
+      fontStyle: "normal",
+      fontWeight: "500",
+      lineHeight: "normal",
+    },
+    bodyBold: {
+      fontFamily: "Poppins",
+      fontSize: "16px",
+      fontStyle: "normal",
+      fontWeight: "500",
+      lineHeight: "normal",
+    },
+  },
+  regular: {
+    displayBold: {
+      fontFamily: "Poppins",
+      fontSize: "48px",
+      fontStyle: "normal",
+      fontWeight: "400",
+      lineHeight: "normal",
+    },
+    headingBold: {
+      fontFamily: "Poppins",
+      fontSize: "36px",
+      fontStyle: "normal",
+      fontWeight: "400",
+      lineHeight: "normal",
+    },
+    titleBold: {
+      fontFamily: "Poppins",
+      fontSize: "24px",
+      fontStyle: "normal",
+      fontWeight: "400",
+      lineHeight: "normal",
+    },
+    bodyBold: {
+      fontFamily: "Poppins",
+      fontSize: "16px",
+      fontStyle: "normal",
+      fontWeight: "400",
+      lineHeight: "normal",
+    },
+  },
+};
+
+export { colorStyle, textStyles };
