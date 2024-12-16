a8d9f2e2d58875154f031d7f2f72bfb5e39d4ebe
carrington-115 <frumarkcarrington@gmail.com>
2024-12-16 11:40:27 +0530

    updated all the pages

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function componentName() {
   return (
     <>
-      <h2>Programmes</h2>
+      <h2>Programs</h2>
     </>
   );
 }
--- contants.ts
+++ contants.ts
@@ -5,7 +5,7 @@ import { linkElementType } from "./@globaltypes";
 const pages: linkElementType[] = [
   { id: 1, link: "/", linkName: "Home" },
   { id: 2, link: "/about", linkName: "About us" },
-  { id: 3, link: "/programmes", linkName: "Programmes" },
+  { id: 3, link: "/programs", linkName: "Programs" },
   { id: 4, link: "/impact", linkName: "Impact" },
   { id: 5, link: "/get-involved", linkName: "Get involved" },
   { id: 6, link: "/contact", linkName: "Contact us" },
